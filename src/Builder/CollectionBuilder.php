8f698d3f5186e5c9051b70d1e9377d3003c04b32
Vincent van Dijk <info@vincentvandijk.com>
2020-03-05 18:40:18 +0100

    Added CollectionBuilder.

---
+++ src/Builder/CollectionBuilder.php
@@ -0,0 +1,83 @@
+<?php
+declare(strict_types=1);
+
+namespace ExtendsFramework\Hateoas\Builder;
+
+use ExtendsFramework\Hateoas\Attribute\Attribute;
+use ExtendsFramework\Hateoas\Link\Link;
+use ExtendsFramework\Router\RouterException;
+use ExtendsFramework\Router\RouterInterface;
+
+class CollectionBuilder extends Builder
+{
+    /**
+     * CollectionBuilder constructor.
+     *
+     * @param RouterInterface $router
+     * @param string $route
+     * @param int $limit
+     * @param int $page
+     * @param int $total
+     * @param array $resources
+     * @param string $rel
+     * @param array|null $keys
+     *
+     * @throws RouterException
+     */
+    public function __construct(
+        RouterInterface $router,
+        string $route,
+        int $limit,
+        int $page,
+        int $total,
+        array $resources,
+        string $rel,
+        array $keys = null
+    ) {
+        $keys['limit'] = $keys['limit'] ?? 'limit';
+        $keys['page'] = $keys['page'] ?? 'page';
+        $keys['total'] = $keys['total'] ?? 'total';
+
+        $this
+            ->addLink(
+                'self',
+                new Link(
+                    $router->assemble($route, [
+                        $keys['limit'] => $limit,
+                        $keys['page'] => $page,
+                    ])
+                )
+            )
+            ->addAttribute($keys['limit'], new Attribute($limit))
+            ->addAttribute($keys['page'], new Attribute($page))
+            ->addAttribute($keys['total'], new Attribute($total));
+
+        if ($page > 1) {
+            $this->addLink(
+                'prev',
+                new Link(
+                    $router->assemble($route, [
+                        $keys['limit'] => $limit,
+                        $keys['page'] => $page - 1,
+                    ])
+                )
+            );
+        }
+
+        if (($page * $limit) < $total) {
+            $this->addLink(
+                'next',
+                new Link(
+                    $router->assemble($route, [
+                        $keys['limit'] => $limit,
+                        $keys['page'] => $page + 1,
+                    ])
+                )
+            );
+        }
+
+        foreach ($resources as $resource) {
+            $this->addResource($rel, $resource, false);
+        }
+    }
+}
